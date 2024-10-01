9d5dd8ca214b8069d7be71652c6f1ec46d769a14
Vlad Dementev <dementvlad88@gmail.com>
2024-10-02 01:00:57 +0300

    VLAD 7: add leetcode1929

---
+++ java/leetcode/leetcode1929.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+public class leetcode1929 {
+    class Solution {
+
+        public int[] getConcatenation(int[] nums) {
+            int[] arr = new int[nums.length*2];
+
+            for (int i = 0 ; i < nums.length ;i++ ){
+                arr[i] = nums[i];
+                arr[i+ nums.length] =nums[i];
+                
+            }
+            return arr;
+        }
+    }
+
+}
